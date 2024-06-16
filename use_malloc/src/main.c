3c9a5faefe0840fec4471025a08e06959bece5a6
HimekaTsuchida <yuria.soft0615@gmail.com>
2024-06-16 04:48:24 +0000

    formatter settings change

--- use_malloc/src/main.c
+++ use_malloc/src/main.c
@@ -1,6 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/*
+要件
+1. ユーザーに標準入力で任意の数字(a)を書き込ませる
+2. a回任意の数字を聞く(b_list)
+3. 標準出力でb_listを表示する
+*/
 
 int main() {
-  printf("Hello, World!\n");
+  int N;
+  printf("please set a number:");
+  scanf("%d", &N);
+  int *num_list = malloc(N * sizeof(int));
+  if (num_list == NULL) {
+    printf("Memory allocation faild");
+  }
+  for (int i = 0; i < N; i++) {
+    printf("Please enter number %d:", i + 1);
+    scanf("%d", &num_list[i]);
+  }
+  printf("You entered reverse:");
+  for (int i = 0; i < N; i++) {
+    printf("%d", num_list[N - i]);
+    print("\n");
+  }
+  free(num_list);
   return 0;
 }
