d4623506653464bcea4b3296c4cc9d6376a0bd65
HimekaTsuchida <yuria.soft0615@gmail.com>
2025-04-05 19:38:51 +0900

    ２分木探索木を作成

--- 10_tree/src/main.cpp
+++ 10_tree/src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <tuple>
 #define DEFAULT_N 100
 
 struct tree_node {
@@ -26,10 +27,90 @@ class tree {
     }
     delete this_node;
   }
+  void insert_node(tree_node *this_node, int64_t value) {
+    if (this_node->value == value) {
+      return;
+    }
+    if (this_node->value > value) {
+      if (this_node->left_node == nullptr) {
+        tree_node(value, nullptr, nullptr);
+        return;
+      }
+      insert_node(this_node->left_node, value);
+      return;
+    }
+    if (this_node->value < value) {
+      if (this_node->right_node == nullptr) {
+        tree_node(value, nullptr, nullptr);
+        return;
+      }
+      insert_node(this_node->right_node, value);
+      return;
+    }
+    // 来ないはずなのでErrorにしてもいいがreturnを書く
+    return;
+  }
+  std::tuple<bool, tree_node *> search_node(tree_node *this_node,
+                                            int64_t value) {
+    if (this_node->value == value) {
+      return {true, nullptr};
+    }
+    if (this_node->left_node == nullptr && this_node->right_node == nullptr) {
+      return {false, nullptr};
+    }
+    if (this_node->value > value) {
+      return {false, this_node->left_node};
+    }
+    if (this_node->value < value) {
+      return {false, this_node->right_node};
+    }
+    throw "error";
+  }
+
+  std::tuple<bool, tree_node *> node_serch(int64_t value) {
+    tree_node this_node = *root_node;
+    while (true) {
+      auto [res, next] = search_node(&this_node, value);
+      if (res == true) {
+        return {true, &this_node};
+      }
+      if (next == nullptr) {
+        return {false, nullptr};
+      }
+      this_node = *next;
+    }
+  }
+  std::tuple<tree_node *, tree_node *> get_max_node(tree_node *target_tree) {
+    if (target_tree->right_node == nullptr) {
+      return {target_tree, nullptr};
+    }
+    auto [res, next] = get_max_node(target_tree->right_node);
+    if (res != nullptr && next == nullptr) {
+      return {res, target_tree};
+    }
+    return {res, next};
+  }
 
  public:
   explicit tree(int64_t value) { new tree_node(value, nullptr, nullptr); }
   ~tree() { delete_node(root_node); }
+  void insert(int64_t value) { insert_node(root_node, value); }
+  bool serch(int64_t value) {
+    auto [res, _] = node_serch(value);
+    return res;
+  }
+  void delete_node(int64_t value) {
+    auto [res, target] = node_serch(value);
+    if (res == false) {
+      throw "not found";
+    }
+    if (target->left_node == nullptr && target->right_node == nullptr) {
+      delete target;
+    }
+    auto [new_node, new_node_per] = get_max_node(target->left_node);
+    target->value = new_node->value;
+    new_node_per->left_node = new_node->left_node;
+  }
 };
 
-int main() { return 0; }
+int main() {}
